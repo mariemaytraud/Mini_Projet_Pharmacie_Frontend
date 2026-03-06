fcfde840972ee009663b7d8e1e82f9e5462e95c5
mariemaytraud <maytraudmarie@gmail.com>
2026-03-06 19:13:23 +0100

    encore des modifs

--- src/model/Medicament.js
+++ src/model/Medicament.js
@@ -2,16 +2,17 @@
 export class Medicament {
   constructor (medicamentJSON) { // en paramètre un médicament au format JSON
   
-    // 1. L'astuce pour récupérer l'ID caché par Spring Boot
-    if (jsonMed._links && jsonMed._links.self) {
-      const url = jsonMed._links.self.href;
+    // pour récupérer l'ID caché par Spring Boot
+    if (medicamentJSON._links && medicamentJSON._links.self) {
+      const url = medicamentJSON._links.self.href;
+      url = url.split('{')[0]; // On enlève la partie entre accolades
       const urlCoupee = url.split('/');
       this.id = urlCoupee[urlCoupee.length - 1]; // On prend le dernier chiffre de l'URL
     } else {
-      this.id = jsonMed.id; 
+      this.id = medicamentJSON.id; 
     }
   
-  this._id = medicamentJSON.reference 
+  //this._id = medicamentJSON.reference 
   this._nom = medicamentJSON.nom // on met un nom plus simple que dans le json
   this._quantiteParUnite = medicamentJSON.quantiteParUnite
   this._quantiteStock = medicamentJSON.unitesEnStock

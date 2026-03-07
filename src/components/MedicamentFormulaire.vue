6ddc9b98f444b81652215639359ea9366100dd5e
mariemaytraud <maytraudmarie@gmail.com>
2026-03-07 12:52:53 +0100

    l formulaire devient fenetre popup

--- src/components/MedicamentFormulaire.vue
+++ src/components/MedicamentFormulaire.vue
@@ -1,17 +1,36 @@
 <template>
-    <v-card>
-        <v-card-title>Nouveau médicament</v-card-title>
-       
-        <v-form @submit.prevent="ajouter">
-            <v-text-field v-model="nom" label="Nom"></v-text-field>
-            <v-text-field v-model="forme" label="Forme (ex: Gélules)"></v-text-field>
-            <v-text-field v-model="stock" label="Stock" type="number"></v-text-field>
-            <v-text-field v-model="photo" label="Lien photo"></v-text-field>
-        
-        <v-btn type="submit" color="blue">Ajouter</v-btn>
+  <v-dialog max-width="500">
     
-    </v-form>
-  </v-card>
+    <template v-slot:activator="{ props: activatorProps }">
+      <v-btn
+        v-bind="activatorProps"
+        color="blue"
+        prepend-icon="mdi-plus"
+        class="mb-4"
+      >
+        Nouveau Médicament
+      </v-btn>
+    </template>
+
+    <template v-slot:default="{ isActive }">
+      <v-card title="Ajouter un médicament">
+        <v-card-text>
+          <v-form @submit.prevent="ajouter(isActive)">
+            <v-text-field v-model="nom" label="Nom" variant="outlined" class="mb-2"></v-text-field>
+            <v-text-field v-model="forme" label="Forme (ex: Gélules)" variant="outlined" class="mb-2"></v-text-field>
+            <v-text-field v-model="stock" label="Stock" type="number" variant="outlined" class="mb-2"></v-text-field>
+            <v-text-field v-model="photo" label="Lien photo" variant="outlined"></v-text-field>
+            
+            <v-card-actions>
+              <v-spacer></v-spacer> <v-btn text="Annuler" variant="plain" @click="isActive.value = false"></v-btn>
+              <v-btn color="blue" type="submit" variant="flat">Ajouter</v-btn>
+            </v-card-actions>
+          </v-form>
+        </v-card-text>
+      </v-card>
+    </template>
+    
+  </v-dialog>
 </template>
 
 <script setup>
@@ -24,20 +43,25 @@ const photo = ref("");
 
 const emit = defineEmits(["addMed"]);
 
-function ajouter() {
-    const nouveauMedicament = {
-        nom: nom.value,
-        quantiteParUnite: forme.value,
-        quantiteEnStock: parseInt(stock.value),
-        imageURL: photo.value
-    };
+//reçoit "isActive" depuis le HTML pour pouvoir ordonner la fermeture
+function ajouter(isActive) {
+  const nouveauMedicament = {
+    nom: nom.value,
+    quantiteParUnite: forme.value,
+    unitesEnStock: parseInt(stock.value),
+    photo: photo.value
+  };
 
-    emit("addMed", nouveauMedicament);
+  //envoie au parent
+  emit("addMed", nouveauMedicament);
 
-    nom.value = "";
-    forme.value = "";
-    stock.value = 0;
-    photo.value = "";
+  //vide les champs
+  nom.value = "";
+  forme.value = "";
+  stock.value = 0;
+  photo.value = "";
+  
+  //ferme la popup automatiquement
+  isActive.value = false;
 }
 </script>
-
